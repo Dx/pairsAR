b88a552740936d3c1a6d1f2fb142baa21a14f2bf
Dx <dequis@gmail.com>
2023-06-11 19:33:41 -0600

    apenas voltea las tarjetas

--- PairsAR/ViewController.swift
+++ PairsAR/ViewController.swift
@@ -15,10 +15,43 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let anchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.2, 0.2])
+        arView.scene.addAnchor(anchor)
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        var cards: [Entity] = []
+        for _ in 1...4 {
+            let box = MeshResource.generateBox(width: 0.04, height: 0.002, depth: 0.04)
+            let metalMaterial = SimpleMaterial(color: .gray, isMetallic: true)
+            let model = ModelEntity(mesh: box, materials: [metalMaterial])
+            
+            model.generateCollisionShapes(recursive: true)
+            cards.append(model)
+        }
+        
+        for (index, card) in cards.enumerated() {
+            let x = Float(index % 2)
+            let z = Float(index / 2)
+            
+            card.position = [x*0.1, 0, z*0.1]
+            anchor.addChild(card)
+        }
+    }
+    
+    
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        let tapLocation = sender.location(in: arView)
+        if let card = arView.entity(at: tapLocation) {
+            if card.transform.rotation.angle == .pi {
+                var flipDownTransform = card.transform
+                flipDownTransform.rotation = simd_quatf(angle:0, axis: [1, 0, 0])
+                card.move(to: flipDownTransform, relativeTo: card.parent, duration: 0.25, timingFunction: .easeInOut)
+            } else {
+                var flipUpTransform = card.transform
+                flipUpTransform.rotation = simd_quatf(angle: .pi, axis: [1, 0,0])
+                card.move(to: flipUpTransform, relativeTo: card.parent, duration: 0.25, timingFunction: .easeInOut)
+            }
+        }        
     }
 }
+
+//https://youtu.be/mV7SWeMDEfg?t=747
